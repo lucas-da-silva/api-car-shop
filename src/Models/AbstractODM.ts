c69b4c20e1885d66d96512d5fbc11c63092c488c
lucasdasilva <xBigodinho@outlook.com>
2023-02-13 15:38:58 -0300

    feat: create Abstract and Car ODMs

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,15 @@
+import { model, Model, models, Schema } from 'mongoose';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+}
+
+export default AbstractODM;
