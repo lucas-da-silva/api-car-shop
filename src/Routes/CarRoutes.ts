23053f53e52cb06afaca18eebd9a3a0509a6c213
lucasdasilva <xBigodinho@outlook.com>
2023-02-13 18:07:08 -0300

    feat: create GET /cars and GET /cars/:id routes

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -8,4 +8,14 @@ routes.post(
   (req, res, next) => new CarController(req, res, next).create(),
 );
 
+routes.get(
+  '/',
+  (req, res, next) => new CarController(req, res, next).getAllCars(),
+);
+
+routes.get(
+  '/:id',
+  (req, res, next) => new CarController(req, res, next).getByIdCar(),
+);
+
 export default routes;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
 import Routes from './Routes/Routes';
 
 const app = express();
 app.use(express.json());
 app.use(Routes);
+app.use(ErrorHandler.handle);
 
 export default app;
