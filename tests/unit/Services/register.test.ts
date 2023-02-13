312d43a0c98d0e983f97084475dcd1418ac45d79
lucasdasilva <xBigodinho@outlook.com>
2023-02-13 19:20:05 -0300

    test: create tests for register method

---
+++ tests/unit/Services/register.test.ts
@@ -0,0 +1,27 @@
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/CarService';
+import ICar from '../../../src/Interfaces/ICar';
+
+describe('Should create a car', function () {
+  it('should successfully create a car', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput = new Car({ id: '6348513f34c397abcad040b2', ...carInput });
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const resolves = await service.register(carInput);
+    
+    expect(resolves).to.be.deep.equal(carOutput);
+  });
+});
