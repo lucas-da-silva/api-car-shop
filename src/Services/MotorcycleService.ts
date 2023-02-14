7b8b1384ed105e73e9c2a77a272d7899e9f7a40d
lucasdasilva <xBigodinho@outlook.com>
2023-02-14 09:51:13 -0300

    feat: create MotorcycleService and register method inside it

---
+++ src/Services/MotorcycleService.ts
@@ -0,0 +1,23 @@
+import Motorcycle from '../Domains/Motorcycle';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleODM from '../Models/MotorcycleODM';
+// import CustomError from '../utils/CustomError';
+
+class MotorcycleService {
+  private motorcycleODM: MotorcycleODM;
+
+  constructor() {
+    this.motorcycleODM = new MotorcycleODM();
+  }
+
+  private createMotorcycleDomain(car: IMotorcycle): Motorcycle {
+    return new Motorcycle(car);
+  }
+
+  public async register(data: IMotorcycle) {
+    const newMotorcycle = await this.motorcycleODM.create(data);
+    return this.createMotorcycleDomain(newMotorcycle);
+  }
+}
+
+export default MotorcycleService;
