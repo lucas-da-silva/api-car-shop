513257d2492b7ef29c0839a6215f19ac53d8364d
lucasdasilva <xBigodinho@outlook.com>
2023-02-14 10:42:47 -0300

    feat: create Erros and HttpStatus enums

---
+++ src/utils/Errors.ts
@@ -0,0 +1,6 @@
+enum Errors {
+  CAR_NOT_FOUND = 'Car not found',
+  INVALID_MONGO_ID = 'Invalid mongo id',
+}
+
+export default Errors;
---
+++ src/utils/HttpStatus.ts
@@ -0,0 +1,8 @@
+enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  NOT_FOUND = 404,
+  UNPROCESSABLE_ENTITY = 422,
+}
+
+export default HttpStatus;
