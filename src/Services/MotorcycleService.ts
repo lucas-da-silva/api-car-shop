05428cf17638f2d602c6e997931c6af333454615
lucasdasilva <xBigodinho@outlook.com>
2023-02-14 11:17:36 -0300

    feat: create getAllMotorcycle and getByIdMotorcycle methods

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,6 +1,9 @@
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
+import CustomError from '../utils/CustomError';
+import HttpStatus from '../utils/HttpStatus';
+import MessageError from '../utils/MessageError';
 // import CustomError from '../utils/CustomError';
 
 class MotorcycleService {
@@ -10,13 +13,26 @@ class MotorcycleService {
     this.motorcycleODM = new MotorcycleODM();
   }
 
-  private createMotorcycleDomain(car: IMotorcycle): Motorcycle {
-    return new Motorcycle(car);
+  private createMotorcycleDomain(motorcycle: IMotorcycle): Motorcycle {
+    return new Motorcycle(motorcycle);
   }
 
-  public async registerMotorcycle(data: IMotorcycle) {
+  public async registerMotorcycles(data: IMotorcycle) {
     const newMotorcycle = await this.motorcycleODM.create(data);
     return this.createMotorcycleDomain(newMotorcycle);
+  }
+
+  public async getAllMotorcycle() {
+    const motorcycles = await this.motorcycleODM.find();
+    return motorcycles.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
+  }
+
+  public async getByIdMotorcycle(id: string) {
+    const motorcycle = await this.motorcycleODM.findById(id);
+    if (!motorcycle) { 
+      throw new CustomError(HttpStatus.NOT_FOUND, MessageError.CAR_NOT_FOUND);
+    }
+    return this.createMotorcycleDomain(motorcycle);
   }
 }
 
